3cff3a6f0667c220b87ad3e79f5cd3507ede5641
Arunprakash M <101006090+arunprakash17@users.noreply.github.com>
2023-08-08 08:22:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,6 +10,6 @@ ScrollReveal({
  ScrollReveal().reveal('.navbar-brand,.btn btn-secondary,.carousel-inner,.btn,.pay-btn,.telegram,.events,.footer,.list-contents,.num',{origin: 'top'});
  ScrollReveal().reveal('.btn btn-danger btn-sm,.evt-date,.news-head,.card-text,.loc', {origin: 'bottom'});
  ScrollReveal().reveal('.container-box,.foot-contact,.carousel-inner,.circle,.pay-head,.card-group,.h2,.title,.links,.container,.pay-head', {origin: 'left'});
- ScrollReveal().reveal('.box,.news,.pay-link,.box,.right d-flex justify-content-around mx-auto,.profiles,.events-right,.right d-flex justify-content-around mx-auto,.logo1,.card,.foot-contact,.details', {origin: 'right'});
+ ScrollReveal().reveal('.box,.news,.pay-link,.box,.right d-flex justify-content-around mx-auto,.profiles,.events-right,.right d-flex justify-content-around mx-auto,.logo1,.card,.foot-contact,.details', {origin: 'left'});
  
  
